911d469c40c22525015bb6f61f0eae4dc22034a4
José Flores <josecruzflores.fl@gmail.com>
2018-10-27 00:22:19 -0500

    Add test for a simple operation, ignored characters and decimals

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,16 +1,34 @@
 import pytest
 from ExpressionEvaluator import lexer
+from decimal import Decimal, getcontext
+getcontext().prec = 4
 
 
 def token_to_tuple(token):
     return (token.type, token.value)
 
 
-def lexer_to_list(lex):
+def lexer_to_list():
     return [token_to_tuple(token) for token in lexer]
 
 
 def test_one_operator():
     lexer.input('+')
-    assert lexer_to_list(lexer) == [('PLUS', '+')]
+    assert lexer_to_list() == [('PLUS', '+')]
 
+
+def test_simple_operation():
+    lexer.input('3+5')
+    assert lexer_to_list() == [('INT', 3), ('PLUS', '+'), ('INT', 5)]
+
+
+def test_character_ignore():
+    lexer.input('3 + \t\n 5')
+    assert lexer_to_list() == [('INT', 3), ('PLUS', '+'), ('INT', 5)]
+
+
+def test_decimal():
+    lexer.input('2.15 + 3 - 4.2')
+    assert lexer_to_list() == [('DECIMAL', Decimal('2.15')), ('PLUS', '+'),
+                               ('INT', 3), ('MINUS', '-'),
+                               ('DECIMAL', Decimal('4.2'))]
