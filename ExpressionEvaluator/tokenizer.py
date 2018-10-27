ab4b47b7b36ec9fa7d42a808310540c24c471d06
José Flores <josecruzflores.fl@gmail.com>
2018-10-26 23:57:27 -0500

    Add rules to lexer and create an instance

--- ExpressionEvaluator/tokenizer.py
+++ ExpressionEvaluator/tokenizer.py
@@ -1,5 +1,32 @@
 import ply.lex as lex
+from decimal import Decimal, getcontext
+
+getcontext().prec = 4
 
 tokens = ('PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'RPAREN', 'LPAREN', 'VAR', 'INT',
           'DECIMAL', 'EXP')
 
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_DIVIDE = r'/'
+t_TIMES = r'\*'
+t_EXP = r'\^'
+t_RPAREN = r'\('
+t_LPAREN = r'\)'
+t_VAR = r'[a-zA-Z][a-zA-Z_0-9]*'
+
+
+
+def t_INT(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
+
+
+def t_DECIMAL(t):
+    r'[0-9]+.[0-9]+'
+    t.value = Decimal(t.value)
+
+t_ignore = ' \t\n\r'
+
+lexer = lex.lexer()
