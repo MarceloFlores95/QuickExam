58646ac810f7e49476051fe8b2275711cc98b85f
José Flores <josecruzflores.fl@gmail.com>
2018-10-28 18:28:44 -0600

    Reformat db.py

--- database/db.py
+++ database/db.py
@@ -8,18 +8,22 @@ Base = declarative_base()
 
 # Define database tables
 
+
 class Subject(Base):
     __tablename__ = 'Subject'
     # Define columns for the Subject table
     # subject_id = Column(Integer, primary_key=True)    # name is now the PK
     name = Column(String(100), nullable=False, primary_key=True)
 
+
 class Topic(Base):
     __tablename__ = 'Topic'
     # topic_id = Column(Integer, primary_key=True)  # name and subject is now the PK
     name = Column(String(100), nullable=False, primary_key=True)
-    subject_name = Column(Integer, ForeignKey('Subject.name'), nullable=False, primary_key=True)
+    subject_name = Column(
+        Integer, ForeignKey('Subject.name'), nullable=False, primary_key=True)
     subject = relationship(Subject)
+
 
 class QuestionOpen(Base):
     __tablename__ = 'QuestionOpen'
@@ -37,6 +41,7 @@ class QuestionTF(Base):
     topic_name = Column(Integer, ForeignKey('Topic.name'), nullable=False)
     topic = relationship(Topic)
 
+
 class QuestionMulti(Base):
     __tablename__ = 'QuestionMulti'
     id = Column(Integer, primary_key=True)
@@ -45,20 +50,26 @@ class QuestionMulti(Base):
     topic_name = Column(Integer, ForeignKey('Topic.name'), nullable=False)
     topic = relationship(Topic)
 
+
 class DummyAnswers(Base):
     __tablename__ = 'DummyAnswers'
     id = Column(Integer, primary_key=True)
     answer = Column(String(1000), nullable=False)
-    question_id = Column(Integer, ForeignKey('QuestionMulti.id'), nullable=False)
+    question_id = Column(
+        Integer, ForeignKey('QuestionMulti.id'), nullable=False)
     question = relationship(QuestionMulti)
+
 
 class Test(Base):
     __tablename__ = 'Test'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     header = Column(String(10000), nullable=False)
-    topic_name = Column(String(100), ForeignKey('TestQuestions.topic_name'), nullable=False)
-    subject_name = Column(String(100), ForeignKey('TestQuestions.subject_name'), nullable=False)
+    topic_name = Column(
+        String(100), ForeignKey('TestQuestions.topic_name'), nullable=False)
+    subject_name = Column(
+        String(100), ForeignKey('TestQuestions.subject_name'), nullable=False)
+
 
 class TestQuestions(Base):
     __tablename__ = 'TestQuestions'
@@ -71,5 +82,4 @@ class TestQuestions(Base):
 
     __table_args__ = (ForeignKeyConstraint([topic_name, subject_name],
                                            [Topic.name, Topic.name]),
-                      ForeignKeyConstraint([test_id],
-                                           [Test.id]))
+                      ForeignKeyConstraint([test_id], [Test.id]))
