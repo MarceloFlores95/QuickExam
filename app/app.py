bdbfca2510302a109c3b6cee558773f037c1e010
José Flores <josecruzflores.fl@gmail.com>
2018-11-03 15:35:42 -0600

    Add LoginManager to app

--- app/app.py
+++ app/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///QuickExam.db'
 api = Api(app)
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
 from .models import *
 
