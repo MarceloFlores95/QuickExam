c4a05c111c399cceac85b48b2bd9fb8a8330877c
José Flores <josecruzflores.fl@gmail.com>
2018-11-03 17:30:33 -0600

    Set SWAGGER_UI_JSONEDITOR to True

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///QuickExam.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SWAGGER_UI_JSONEDITOR'] = True
 CORS(app)
 api = Api(app)
 db = SQLAlchemy(app)
