6ab09de21b5017e507d7f7ce8aa205539f0d8092
José Flores <josecruzflores.fl@gmail.com>
2018-11-04 15:55:24 -0600

    Add test related to boolean operations

--- tests/test_boolean_parser.py
+++ tests/test_boolean_parser.py
@@ -8,4 +8,40 @@ def test_comparison_1():
 
 
 def test_comparison_2():
-    assert parser.parse('3<2') == False
+    assert parser.parse('3<3') == False
+
+
+def test_comparison_3():
+    assert parser.parse('3<=3') == True
+
+
+def test_and_1():
+    assert parser.parse('true and true') == True
+
+
+def test_and_2():
+    assert parser.parse('true and false') == False
+
+
+def test_and_3():
+    assert parser.parse('3 > 2 and 4 < 5') == True
+
+
+def test_or_1():
+    assert parser.parse('true or false') == True
+
+
+def test_or_2():
+    assert parser.parse('2 > 3 or 3 > 2') == True
+
+
+def test_or_3():
+    assert parser.parse('2 < 1 or 1 == 2') == False
+
+
+def test_negation_1():
+    assert parser.parse('~true') == False
+
+
+def test_negation_2():
+    assert parser.parse('¬(false)') == True
