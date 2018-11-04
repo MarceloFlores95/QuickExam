42bab94835918e17284b9c6d80841260d8cdc775
José Flores <josecruzflores.fl@gmail.com>
2018-11-03 21:10:07 -0600

    Correct app.py

--- app/app.py
+++ app/app.py
@@ -1,19 +1,18 @@
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(blueprint)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///QuickExam.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SWAGGER_UI_JSONEDITOR'] = True
 CORS(app)
-api = Api(blueprint, doc='/documentation')
-blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(app, doc='/api/documentation')
 db = SQLAlchemy(app)
 
 from .models import *
+from .resources import *
 
 migrate = Migrate(app, db)
