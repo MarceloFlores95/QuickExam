7df6f9176e04b109e4ffe7e81385f1fbaeafaa4b
José Flores <josecruzflores.fl@gmail.com>
2018-11-04 15:22:17 -0600

    Add complete parser functionality

--- evaluator/booleanParser.py
+++ evaluator/booleanParser.py
@@ -21,9 +21,8 @@ class BooleanParser:
         'term: term AND boolean'
         p[0] = p[1] and p[3]
 
-
     def p_term_boolean(self, p):
-        'boolean: term'
+        'term: boolean'
         p[0] = p[1]
 
     def p_boolean_true(self, p):
@@ -41,3 +40,39 @@ class BooleanParser:
     def p_boolean_expression(self, p):
         'boolean: LPAREN expression RPAREN'
         p[0] = p[2]
+
+    def p_boolean_lt(self, p):
+        'boolean: factor LT factor'
+        p[0] = p[1] < p[3]
+
+    def p_boolean_gt(self, p):
+        'boolean: factor GT factor'
+        p[0] = p[1] > p[3]
+
+    def p_boolean_eq(self, p):
+        'boolean: factor EQ factor'
+        p[0] = p[1] == p[3]
+
+    def p_boolean_lte(self, p):
+        'boolean: factor LTE factor'
+        p[0] = p[1] <= p[3]
+
+    def p_boolean_gte(self, p):
+        'boolean: factor GTE factor'
+        p[0] = p[1] >= p[3]
+
+    def p_factor_var(self, p):
+        'factor: VAR'
+        p[0] = p[1]
+
+    def p_factor_int(self, p):
+        'factor: INT'
+        p[0] = p[1]
+
+    def p_factor_decimal(self, p):
+        'factor: DECIMAL'
+        p[0] = p[1]
+
+    def p_negative_factor(self, p):
+        'factor: MINUS factor'
+        p[0] = -p[2]
