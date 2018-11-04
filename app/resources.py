b5890ece213c41423a55380f9d862046332bea73
José Flores <josecruzflores.fl@gmail.com>
2018-11-04 11:12:55 -0600

    Use token_check decorator and add documentation to the API

--- app/resources.py
+++ app/resources.py
@@ -21,17 +21,18 @@ def create_token(user):
 
 def token_check(func):
     def wrapper(*args, **kwargs):
-        token = flask.request.args.headers('X-API-KEY')
+        token = flask.request.headers.get('X-API-KEY')
         try:
             data = jwt.decode(token, SECRET)
         except:
             return {'message': 'Invalid token'}, 401
-        return func(user_id=data['user_id'], *args, **kwargs)
+        return func(*args, user_id=data['user_id'], **kwargs)
 
     return wrapper
 
 
 @api.route('/api/login')
+@api.doc(params={'username' : 'The username', 'password' : 'The password'})
 class Token(Resource):
     def post(self):
         user_data = user_parser.parse_args()
@@ -50,54 +51,46 @@ class UserRegister(Resource):
         user.set_password(user_data['password'])
         db.session.add(user)
         db.session.commit()
-        token = create_token(token)
+        token = create_token(user)
         return {'token': token.decode('UTF-8')}
 
 
-@api.route('/api/subject/<string:token>')
+@api.route('/api/subject')
 class SubjectView(Resource):
-    def get(self, token):
-        data = token_check(token)
-        if data:
-            user = User.query.filter_by(id=data['user_id']).first()
-            subjects = [subject.get_parameters() for subject in user.subjects]
-            return subjects
-        else:
-            return {'message': 'Invalid token'}, 401
+    @api.doc(security='apikey')
+    @token_check
+    def get(self, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        subjects = [subject.get_parameters() for subject in user.subjects]
+        return subjects
 
-    def post(self, token):
-        data = token_check(token)
-        if data:
-            subject_data = subject_parser.parse_args()
-            subject = Subject(
-                name=subject_data['name'], user_id=data['user_id'])
-            db.session.add(subject)
-            db.session.commit()
-            return {'message': 'Successfully added Subject'}
-        else:
-            return {'message': 'Invalid token'}, 401
+    @api.doc(security='apikey', params={'name' : "The subject's name"})
+    @token_check
+    def post(self, user_id):
+        subject_data = subject_parser.parse_args()
+        subject = Subject(name=subject_data['name'], user_id=user_id)
+        db.session.add(subject)
+        db.session.commit()
+        return {'message': 'Successfully added Subject'}
 
 
-@api.route('/api/topic/<string:token>')
+@api.route('/api/topic')
 class TopicView(Resource):
-    def get(self, token):
-        data = token_check(token)
-        if data:
-            subject_id = flask.request.args.get('subject_id')
-            subject = Subject.query.filter_by(id=subject_id).first()
-            topics = [topic.get_parameters() for topic in subject.topics]
-            return topics
-        else:
-            return {'message': 'Invalid token'}, 401
 
-    def post(self, token):
-        data = token_check(token)
-        if data:
-            topic_data = topic_parser.parse_args()
-            topic = Topic(
+    @api.doc(security='apikey', params={'subject_id' : "The id of a subject"})
+    @token_check
+    def get(self, user_id):
+        subject_id = flask.request.args.get('subject_id')
+        subject = Subject.query.filter_by(id=subject_id, user_id=user_id).first()
+        topics = [topic.get_parameters() for topic in subject.topics]
+        return topics
+
+    @api.doc(security='apikey', params={'name' : "The topic's name", 'subject_id' : 'A subject id'})
+    @token_check
+    def post(self):
+        topic_data = topic_parser.parse_args()
+        topic = Topic(
                 name=topic_data['name'], subject_id=topic_data['subject_id'])
-            db.session.add(topic)
-            db.session.commit()
-            return {'message': 'Successfully added Topic'}
-        else:
-            return {'message': 'Invalid token'}, 401
+        db.session.add(topic)
+        db.session.commit()
+        return {'message': 'Successfully added Topic'}
