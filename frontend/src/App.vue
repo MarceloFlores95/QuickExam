8ba064beda5a086d2b00abf89c5f4a100dd899d2
Luis Marcelo Flores Canales <marceloflores@MacBook-Air-de-Luis.local>
2018-11-05 20:55:30 -0600

    Pagina muestra de reactivos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,37 @@
 <template>
+<v-app>
   <div id="app">
-    <img src="./assets/logo.png">
+        <v-toolbar color="grey darken-1" dark>
+        <v-toolbar-title>
+          Quick Exam
+        </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+
+        <v-flex xs6>
+            <v-btn large >Reactivos</v-btn>
+        </v-flex>
+
+        <v-flex xs6>
+            <v-btn large>Examenes</v-btn>
+        </v-flex>
+
+        </v-layout>
+    </v-container>
     <router-view/>
   </div>
+  </v-app>
 </template>
 
 <script>
@@ -18,6 +47,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Reactivos from '@/components/Reactivos'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HW',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Reactivos',
+      component: Reactivos
     }
   ]
 })
