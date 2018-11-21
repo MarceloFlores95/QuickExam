f0f4e6f7202660fa21995f1d19720e93b05d6fca
Luis Marcelo Flores Canales <marceloflores@MacBook-Air-de-Luis.local>
2018-11-21 12:34:48 -0600

    Final work

--- frontend/src/components/EditarExamen.vue
+++ frontend/src/components/EditarExamen.vue
@@ -7,27 +7,34 @@
             <v-card-title>
             <span class="headline">Editar Examen</span>
             </v-card-title>
-             {{exam}}
-             {{filteredExam}}
-<!--
+<p>Editar nombre del examen</p>
         <v-flex xs12 sm6>
             <v-text-field
                 v-model="name"
-                :label="test.name"
+                :label="filteredExam.name"
             ></v-text-field>
         </v-flex>
-
+<p>Editar encabezado del examen</p>
         <v-flex xs12 sm6>
             <v-text-field
                 v-model="encabezado"
-                :label="test.encabezado"
+                :label="filteredExam.encabezado"
             ></v-text-field>
         </v-flex>
--->
+        <p>Cantidad de preguntas</p>
+        <v-flex xs12 sm4>
+        <v-overflow-btn
+          :items="cantidad"
+          target="#dropdown-example"
+          item-value="text"
+          v-model="cantidad"
+        ></v-overflow-btn>
+        </v-flex >
+
         <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="green darken-1" flat @click.native="dialog = false">Cancelar</v-btn>
-            <v-btn color="green darken-1" flat v-on:click="editOpenQuestion(questionOpenId, text, topicId, pregunta.variables)" >Guardar</v-btn>
+            <v-btn color="green darken-1" flat v-on:click="editExam(name, encabezado, cantidad)" >Guardar</v-btn>
         </v-card-actions>
 
         </v-card>
@@ -46,7 +53,8 @@ export default {
       respuestaOM: undefined,
       dummyAnswers: [],
       dummyAnswer: undefined,
-      variables: []
+      variables: [],
+      cantidad: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     }
   },
   components: {
@@ -61,15 +69,11 @@ export default {
     }
   },
   methods: {
-    saveOpenQuestion (pregunta, topicId, tipoPregunta) {
-      console.log(this.variables)
-      let payload = [pregunta, topicId, tipoPregunta, this.variables]
-      this.variables = []
-      this.$store.dispatch('addQuestion', payload)
+    editExam (nombre, encabezado, cantidad) {
+      let payload = [nombre, encabezado, this.selectedExam, cantidad]
+      // console.log(payload)
+      this.$store.dispatch('editExam', payload)
         .then((response) => {
-          console.log('Refrescar variables')
-          console.log(this.variables)
-          this.reset = true
           this.dialog = false
         })
         .catch((error) => {
@@ -82,9 +86,9 @@ export default {
     filteredExam () {
       var EExam = this.exam
       EExam = EExam.filter((YaBasta) => {
-        return YaBasta.test_id === this.selectedTest
+        return YaBasta.test_id === this.selectedExam
       })
-      return EExam
+      return EExam[0]
     }
   }
 }
